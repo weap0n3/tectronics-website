851de41f6d7c35736d4125c2d6d2954e4db46c40
Oleksii Bilyi <weap0n1823@gmail.com>
2026-01-22 16:00:27 +0100

    updated ProductsList

--- src/pages/productList.ts
+++ src/pages/productList.ts
@@ -1,21 +1,14 @@
-export interface Product {
-	id: number
-	name: string
-	price: number
-	photo: string
-	description: string
-	link: string
-}
+import { Product } from '@/types/product.interface'
 
-export const products: Product[] = [
-	{
+export const PRODUCTS: Product[] = [
+	/*{
 		id: 1,
 		name: 'Synology DiskStation DS218',
 		price: 50.99,
 		photo:
 			'https://images.unsplash.com/photo-1577538926988-7926820ed209?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
 		description:
-			'Leistungsstarkes und vielseitiges NAS (Network Attached Storage) mit 2 Festplattenschächten. Das Gerät ist mit einem 64-Bit-Quad-Core-Prozessor mit 1,4 GHz und 2 GB DDR4-RAM ausgestattet. Die Konnektivität umfasst einen USB 2.0-Anschluss, zwei USB 3.0-Anschlüsse und einen 1GbE (RJ-45) LAN-Anschluss. Konzipiert für Privatanwender und kleine Büros, um Daten effizient zu verwalten und zu schützen',
+			'Leistungsstarkes und vielseitiges NAS (Network Attached Storage) mit 2 Festplattenschächten. Das Gerät ist mit einem 64-Bit-Quad-Core-Prozessor mit 1,4 GHz und 2 GB DDR4-RAM ausgestattet. Die Konnektivität umfasst einen USB 2.0-Anschluss, zwei USB 3.0-Anschlüsse und einen 1GbE (RJ-45) LAN-Anschluss. Konzipiert für Privatanwender und kleine Büros, um Daten effizient zu verwalten und zu schützen.',
 		link: '',
 	},
 	{
@@ -56,6 +49,43 @@ export const products: Product[] = [
 			'https://images.unsplash.com/photo-1650094980833-7373de26feb6?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
 		description:
 			'Das  leistungsstarkes A4-Farb-Multifunktionssystem der Canon i-SENSYS MF640 Serie bietet Druck-, Kopier-, Scan- und optional Faxfunktionen in einem kompakten Gehäuse, ideal für kleine Büroumgebungen. Das System erreicht Druckgeschwindigkeiten von bis zu 21 A4-Seiten pro Minute in Farbe und Schwarzweiß. Die Bedienung erfolgt über einen intuitiven 12,7 cm großen LCD-Farb-Touchscreen. Flexible Konnektivitätsoptionen wie USB, LAN und WLAN sowie mobile Kompatibilität über Apps wie Canon PRINT Business sind vorhanden.',
+		link: '',
+	},
+	*/
+	{
+		id: 6,
+		name: 'Microsoft Office 365 Ultra halbjährlich',
+		price: 408.33,
+		photo: 'Office365Ultra.jpg',
+		description:
+			'Microsoft Office 365 Ultra bietet eine umfassende Suite von Produktivitätsanwendungen und Cloud-Diensten für Unternehmen und Einzelpersonen. Mit halbjährlicher Abrechnung ermöglicht es den Zugriff auf Anwendungen wie Word, Excel, PowerPoint und Outlook sowie Cloud-Speicher über OneDrive. Ideal für effizientes Arbeiten und Zusammenarbeit. Halbjährliches Abonnement für Microsoft 365. Mit diesen Paket werden Ihnen nur die Apps zur Verfügung gestellt und 4TB Cloudspeicher. Preis zzgl. Versand- bzw. Transportkosten',
+		link: '',
+	},
+	{
+		id: 7,
+		name: 'Mauspad',
+		price: 27.49,
+		photo: 'GamingMousePad.jpg',
+		description:
+			'Gaming-Mauspad in neuwertigem Zustand und einer glatten Oberfläche für präzise Mausbewegungen und optimaler Kontrolle. Die rutschfeste Unterseite sorgt für Stabilität während intensiver Spielsitzungen. Ideal für Gamer und Bürogebrauch! Perfekt für jedes Setup! Größe: 800 × 300 × 3 mm Preis zzgl. Versand- bzw. Transportkosten',
+		link: '',
+	},
+	{
+		id: 8,
+		name: 'Gaming Tastatur',
+		price: 45.82,
+		photo: 'GamingKeyboard.jpg',
+		description:
+			'Hochwertige Gaming-Tastatur in ausgezeichnetem Zustand! Mechanische Tasten für schnelles und präzises Tippen. RGB-Hintergrundbeleuchtung für individuelles Design. Ergonomisches Design und robuste Bauweise für lange Sessions. Preis zzgl. Versand- bzw. Transportkosten',
+		link: '',
+	},
+	{
+		id: 9,
+		name: 'Firewall (Cisco ASA5508-K9)',
+		price: 353.29,
+		photo: 'FirewallCisco.jpg',
+		description:
+			'Der Cisco ASA 5508-K9 ist eine leistungsstarke, flexible Sicherheits-Appliance, die für kleine bis mittelgroße Unternehmen konzipiert ist. Mit integrierter Firewallschutz, VPN-Funktionalitäten und umfassendem Bedrohungsschutz bietet er umfassende Sicherheitslösungen für Netzwerke. Die Appliance unterstützt eine hohe Anzahl an gleichzeitigen Verbindungen und bietet skalierbare Lizenzoptionen, um den wachsenden Anforderungen gerecht zu werden. Ideal für Unternehmen, die eine robuste Netzwerksicherheit benötigen, kombiniert der ASA 5508-K9 Effizienz mit einfacher Verwaltung über die intuitive Benutzeroberfläche. Preis zzgl. Versand- bzw. Transportkosten',
 		link: '',
 	},
 ]
---
+++ src/types/product.interface.ts
@@ -0,0 +1,8 @@
+export interface Product {
+	id: number
+	name: string
+	price: number
+	photo: string
+	description: string
+	link: string
+}
