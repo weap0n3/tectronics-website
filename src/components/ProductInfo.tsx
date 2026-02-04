c2d143b99d326191405c223c3098251f356762cd
Oleksii Bilyi <weap0n1823@gmail.com>
2026-02-04 17:13:28 +0100

    fixed scroll bug ProductInfo

--- src/components/ProductInfo.tsx
+++ src/components/ProductInfo.tsx
@@ -20,7 +20,7 @@ export const ProductInfo = ({ setIsMoreInfoVisible, product }) => {
 
 	return (
 		<div className='fixed inset-0 bg-black/50 w-full h-full flex items-center justify-center z-50 '>
-			<div className='bg-muted pt-7 pb-10 px-11 rounded-lg w-3/4 max-h-[80vh] overflow-y-auto space-y-5'>
+			<div className='bg-muted pt-7 pb-10 px-11 rounded-lg w-3/4 overflow-y-hidden space-y-5'>
 				<div className='w-full'>
 					<div className='w-fit after:h-px after:bg-primary after:block after:transition-all after:duration-300 after:ease-in-out after:scale-x-0 hover:after:scale-x-100 after:origin-right '>
 						<button
