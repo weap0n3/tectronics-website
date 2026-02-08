cf8a5e19f726650c611b712985d73ef03efc3168
Oleksii Bilyi <weap0n1823@gmail.com>
2026-02-08 17:05:19 +0100

    created backend composer project

---
+++ backend/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'weap0n3/backend',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ed0f542b56b26cfe07a6a8c86f4f4318d99e3cbf',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'weap0n3/backend' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ed0f542b56b26cfe07a6a8c86f4f4318d99e3cbf',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
