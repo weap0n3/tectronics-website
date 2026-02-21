db12cda17f84a900fe0b074a5c02204d09ddb6ed
Oleksii Bilyi <weap0n1823@gmail.com>
2026-02-21 22:24:40 +0100

    ProductInfo adaptive styles

--- frontend/src/components/ProductInfo.tsx
+++ frontend/src/components/ProductInfo.tsx
@@ -20,12 +20,12 @@ export const ProductInfo = ({
 	}, [])
 
 	return (
-		<div className='fixed inset-0 bg-black/50 w-full h-full flex items-center justify-center z-50 '>
-			<div className='bg-muted pt-7 pb-10 px-11 rounded-lg w-3/4 overflow-y-hidden space-y-5'>
+		<div className='fixed inset-0 bg-black/50 w-full h-full flex flex-col lg:flex-row items-center justify-center z-50 '>
+			<div className='bg-muted lg:pt-7 lg:pb-10 lg:px-11 m-8 p-4 rounded-lg lg:w-3/4 lg:overflow-y-hidden overflow-scroll lg:space-y-5 space-y-2'>
 				<div className='w-full'>
-					<div className='w-fit after:h-px after:bg-primary after:block after:transition-all after:duration-300 after:ease-in-out after:scale-x-0 hover:after:scale-x-100 after:origin-right '>
+					<div className='lg:w-fit flex lg:block justify-center lg:justify-normal after:h-px after:bg-primary after:block after:transition-all after:duration-300 after:ease-in-out after:scale-x-0 hover:after:scale-x-100 after:origin-right '>
 						<button
-							className='relative flex flex-wrap justify-center gap-1 items-center hover:text-primary transition-colors'
+							className='relative flex flex-wrap justify-center  gap-2 items-center hover:text-primary transition-colors'
 							onClick={() => setIsMoreInfoVisible(false)}
 						>
 							<ArrowLeft />
@@ -33,29 +33,37 @@ export const ProductInfo = ({
 						</button>
 					</div>
 				</div>
-				<span className='block w-full h-px border'></span>
-				<div className='flex gap-6'>
-					<div className='w-3/5 rounded-lg overflow-hidden'>
+				<span className='block w-full h-px lg:border'></span>
+				<div className='flex flex-col lg:flex-row lg:gap-6 gap-3'>
+					<div className='lg:w-3/5 rounded-lg overflow-hidden'>
 						<img
 							src={product.photo}
 							alt=''
 							className='w-full h-full object-cover'
 						/>
 					</div>
-					<span className='block w-px border'></span>
-					<div className='w-2/5 flex flex-col gap-5 justify-center'>
-						<h1 className='text-5xl font-bold'>{product.name}</h1>
-						<p className='text-4xl font-bold text-primary'>{product.price} €</p>
-						<p className='text-lg'>{product.description}</p>
-						<a href={product.link} target='_blank' className='w-full mt-2'>
-							<Button
-								variant='default'
-								size='lg'
-								className='text-lg w-full text-white hover:scale-105 transition-all duration-300 ease-in-out'
-							>
-								Zum Marketplace
-							</Button>
-						</a>
+					<span className='block w-px lg:border'></span>
+					<div className='lg:w-2/5 flex flex-col lg:gap-5 gap-3 justify-evenly'>
+						<h1 className='lg:text-5xl text-center lg:text-start text-2xl font-bold'>
+							{product.name}
+						</h1>
+						<p className='text-4xl text-center lg:text-start font-bold text-primary'>
+							{product.price} €
+						</p>
+						<div className='flex flex-col-reverse lg:flex-col gap-3'>
+							<p className='text-lg text-center lg:text-start'>
+								{product.description}
+							</p>
+							<a href={product.link} target='_blank' className='w-full mt-2'>
+								<Button
+									variant='default'
+									size='lg'
+									className='text-lg w-full text-white hover:scale-105 transition-all duration-300 ease-in-out'
+								>
+									Zum Marketplace
+								</Button>
+							</a>
+						</div>
 					</div>
 				</div>
 			</div>
