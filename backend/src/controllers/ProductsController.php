db922df9b3033fd35b1120e80f45366be1ae6238
Oleksii Bilyi <weap0n1823@gmail.com>
2026-02-28 22:20:42 +0100

    updated db query

--- backend/src/controllers/ProductsController.php
+++ backend/src/controllers/ProductsController.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../config/db.php';
 class ProductsController {
     public static function getProducts() {
         global $pdo;
-        $stmt = $pdo->query("SELECT id, name, price, photo, description, link FROM products");
+        $stmt = $pdo->query("SELECT id, name, price, discPrice, photo, description, link FROM products");
         $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
         echo json_encode($products);
     }
