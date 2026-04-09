c6a8c6a3331dc6c8ff1aeabd5885876e120f803b
Oleksii Bilyi <weap0n1823@gmail.com>
2026-04-09 21:40:42 +0200

    updated CartSummary

--- frontend/src/components/CartSummary.tsx
+++ frontend/src/components/CartSummary.tsx
@@ -1,44 +1,46 @@
-import { PRODUCTS } from '@/pages/productList'
+import { useCartStore } from '@/store/useCartStore'
+import { MessageSquareWarningIcon } from 'lucide-react'
 import { Link } from 'react-router-dom'
 import { Button } from './ui/button'
 
 export const CartSummary = () => {
 	// TODO: Calculate total price (Product price * quantity)
 
-	const total = PRODUCTS.reduce((sum, item) => {
+	const cartItems = useCartStore(state => state.cart)
+
+	const total = cartItems.reduce((sum, item) => {
 		return sum + item.price * 1
 	}, 0)
 
 	return (
 		<div className='w-full lg:w-1/3 border border-gray-300  rounded-lg p-4 h-fit sticky top-20'>
-			<h2 className='text-2xl font-semibold mb-4'>Summary</h2>
+			<h2 className='text-2xl font-semibold mb-4'>Zusammenfassung</h2>
 
-			<div className='flex justify-between mb-2'>
-				<span>Subtotal</span>
-				<span>${total.toFixed(2)}</span>
-			</div>
-
-			<div className='flex justify-between mb-2'>
-				<span>Shipping</span>
-				<span>$5.00</span>
-			</div>
-
-			<hr className='my-3' />
+			<hr className='mb-4' />
 
 			<div className='flex justify-between font-bold text-lg'>
-				<span>Total</span>
-				<span>${(total + 5).toFixed(2)}</span>
+				<span>Gesamt</span>
+				<span>€ {total.toFixed(2)}</span>
 			</div>
 
-			<div className='mt-4 text-sm text-gray-600'>
-				<p>🚚 Free delivery over $50</p>
-				<p>📦 Delivery: 2–4 days</p>
+			<div className='mt-4 text-sm text-yellow-900 bg-orange-300 border border-orange-950 rounded-lg p-3 text-center'>
+				<div className='flex gap-2 items-center justify-center'>
+					<MessageSquareWarningIcon />
+					<p className='text-lg'>Wichtiger Hinweis</p>
+				</div>
+				<p className='mt-1'>Versandkosten variieren je nach Bestellung</p>
+				<p>
+					Die Versandkosten werden nach Abschluss der Bestellung gesondert in
+					Rechnung gestellt
+				</p>
 			</div>
 
-			<Button className='w-full mt-6'>Proceed to Checkout</Button>
+			<hr className='mt-6' />
+
+			<Button className='w-full mt-6'>Zur Kasse</Button>
 			<Link to='/products'>
 				<Button variant='link' className='w-full mt-3'>
-					Continue Shopping
+					Weiter einkaufen
 				</Button>
 			</Link>
 		</div>
