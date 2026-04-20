bcc23e333e89e4d7de24afd673a8845fc82c4963
Oleksii Bilyi <weap0n1823@gmail.com>
2026-04-20 17:00:02 +0200

    fixed total price bug

--- frontend/src/components/cart/CartSummary.tsx
+++ frontend/src/components/cart/CartSummary.tsx
@@ -5,7 +5,7 @@ import { memo } from 'react'
 import { Link } from 'react-router-dom'
 
 export const CartSummary = memo(() => {
-	const getTotalPrice = useCartStore(state => state.getTotalPrice)
+	const getTotalPrice = useCartStore(state => state.getTotalPrice())
 
 	return (
 		<div className='w-full lg:w-1/3 border border-gray-300  rounded-lg p-4 h-fit sticky top-20'>
@@ -13,10 +13,10 @@ export const CartSummary = memo(() => {
 
 			<div className='flex justify-between text-lg'>
 				<span>Zwischensumme</span>
-				<span>€ {getTotalPrice().toFixed(2)}</span>
+				<span>€ {getTotalPrice.toFixed(2)}</span>
 			</div>
 
-			{getTotalPrice() < 70 && (
+			{getTotalPrice < 70 && (
 				<div className='flex justify-between text-lg'>
 					<span>Liefergebühr</span>
 					<span>€ 10.00</span>
@@ -28,11 +28,11 @@ export const CartSummary = memo(() => {
 			<div className='flex justify-between font-bold text-lg'>
 				<span>Gesamt</span>
 				<span>
-					€ {(getTotalPrice() + (getTotalPrice() < 70 ? 10 : 0)).toFixed(2)}
+					€ {(getTotalPrice + (getTotalPrice < 70 ? 10 : 0)).toFixed(2)}
 				</span>
 			</div>
 
-			{getTotalPrice() < 70 && (
+			{getTotalPrice < 70 && (
 				<div className='mt-4 text-sm text-yellow-900 bg-orange-300 border border-orange-950 rounded-lg p-3 text-center'>
 					<div className='flex gap-2 items-center justify-center'>
 						<MessageSquareWarningIcon />
