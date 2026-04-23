363c8299f81009cf4e11496e4f561062447df3fb
Oleksii Bilyi <weap0n1823@gmail.com>
2026-04-23 22:35:40 +0200

    updated api.php

--- backend/src/routes/api.php
+++ backend/src/routes/api.php
@@ -1,14 +1,29 @@
 <?php
 
 require_once __DIR__ . '/../controllers/ProductsController.php';
+require_once __DIR__ . '/../controllers/OrdersController.php';
+require_once __DIR__ . '/../controllers/UsersController.php';
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 $uri = rtrim($uri, '/');
 
-if ($uri === "/api/products") {
+if ($uri === "/api/getProducts") {
     ProductsController::getProducts();
-} else {
+}
+elseif ($uri === "/api/addOrder" && $_SERVER['REQUEST_METHOD'] === 'POST') {
+    OrdersController::addOrder();
+}
+elseif ($uri === "/api/getUsers" && $_SERVER['REQUEST_METHOD'] === 'GET') {
+    UsersController::getUsers();
+}
+elseif($uri==="/api/addUser" && $_SERVER['REQUEST_METHOD'] === 'POST'){
+    UsersController::addUser();
+}
+elseif($uri === "/api/getUsers/email" && $_SERVER['REQUEST_METHOD'] === 'GET'){
+    UsersController::getUserByEmail();
+}
+else {
     http_response_code(404);
     header('Content-Type: application/json');
     echo json_encode(['error' => 'Route not found']);
