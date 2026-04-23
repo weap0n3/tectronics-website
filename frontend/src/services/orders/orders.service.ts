62b672fb3912c686624dccefe0b24bc6fb654056
Oleksii Bilyi <weap0n1823@gmail.com>
2026-04-23 22:43:33 +0200

    created validateOrder function

--- frontend/src/services/orders/orders.service.ts
+++ frontend/src/services/orders/orders.service.ts
@@ -1,13 +1,23 @@
 import { API_ROUTES } from '@/constants/routes'
-import { IFormData } from '@/types/order.interface'
+import { IUser } from '@/types/order.interface'
 import { IProduct } from '@/types/product.interface'
 import axios from 'axios'
+import { UsersService } from '../users/users.service'
+
+async function addOrder(cart: IProduct[], user: IUser) {
+	await axios
+		.post(API_ROUTES.ADD_ORDER, { cart, user })
+		.catch(err => console.log(err))
+}
 
 export const OrdersService = {
-	async addOrder(cart: IProduct[], customer: IFormData) {
-		const response = await axios
-			.post(API_ROUTES.ADD_ORDER, cart)
-			.catch(err => console.log(err))
-		return response[0]
+	async validateOrder(cart: IProduct[], user: IUser) {
+		let client = await UsersService.getUserByEmail(user.email)
+
+		if (!client) {
+			await UsersService.addUser(user)
+			client = await UsersService.getUserByEmail(user.email)
+		}
+		addOrder(cart, client)
 	},
 }
