b1bab698b07ababf61ff29aa82ee9a2891a997df
Oleksii Bilyi <weap0n1823@gmail.com>
2026-04-23 22:42:01 +0200

    added service getUserByEmail

--- frontend/src/services/users/users.service.ts
+++ frontend/src/services/users/users.service.ts
@@ -21,9 +21,21 @@ export const UsersService = {
 			})
 		return response?.data[0] ?? null
 	},
+	async getUserByEmail(userEmail: string): Promise<IUser | null> {
+		try {
+			const response = await axios.get(
+				`${API_ROUTES.GET_USERS}/email?email=${userEmail}`,
+			)
+
+			return response?.data ?? null
+		} catch (err) {
+			console.error('Error fetching user by ID:', err)
+			return null
+		}
+	},
 	async addUser(user: IUser) {
-		await axios
-			.post(`${API_ROUTES.ADD_USER}`)
+		const response = await axios
+			.post(`${API_ROUTES.ADD_USER}`, user)
 			.catch(err => console.log('Error fetching users:', err))
 	},
 }
